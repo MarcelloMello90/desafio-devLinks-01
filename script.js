e56d6df97d1214708ce3b0006c6b96a001679a54
{Marcelo Melo} <{marcellomello90@gmail.com}>
2023-01-12 20:52:58 +0100

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imgagem
+  if (html.classList.contains("light")) {
+  //se tiver o light mode, adicionar a imagem light
+  img.setAttribute("src","./assets/avatar-light.png")
+  } else {
+  //se tiver sem o light mode, manter a imagem normal
+  img.setAttribute("src","./assets/avatar.png")
+  }
+
+  if (html.classList.contains("light")) {
+    //se tiver o light mode, adicionar a imagem light
+    img.setAttribute("alt", "Foto Marcelo com camisa vermelha sorrindo de oculos escuro")
+  } else {
+    //se tiver sem o light mode, manter a imagem normal
+    img.setAttribute("alt", ".Foto do Marcelo com caminha rosa sorindo")
+  }
+}
